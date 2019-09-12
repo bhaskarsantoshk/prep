8567856836ea76a40d9f6e8cffd9295c9f08c243
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-09-12 19:11:49 +0530

    bitmanipulation/ReverseBits.java

---
+++ bitmanipulation/ReverseBits.java
@@ -0,0 +1,19 @@
+package bitmanipulation;
+
+public class ReverseBits {
+    public long reverse(long a) {
+        long res = 0;
+        for ( int i=0; i<32 ; i++){
+            if ( (a & 1) == 1) {
+                res += 1;
+            }
+            a = a >> 1;
+            res = res << 1;
+        }
+        return res >> 1;
+    }
+    public static void main(String[] args){
+        ReverseBits obj = new ReverseBits();
+        System.out.println(obj.reverse(2));
+    }
+}
