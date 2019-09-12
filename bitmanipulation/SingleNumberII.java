7349fb432b98a52ef3cd9d228a2ff9647c531e73
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-09-12 20:58:43 +0530

    bitmanipulation/SingleNumberII.java

---
+++ bitmanipulation/SingleNumberII.java
@@ -0,0 +1,26 @@
+package bitmanipulation;
+
+import java.util.List;
+
+public class SingleNumberII {
+ /*
+    How to check if a number has set bit in ith position
+     (num & ( 1 << i)) !=0
+    How to set ith position of a number?
+     ( num | ( 1 << i) )
+
+    */
+    public int singleNumber(final List<Integer> A) {
+        int result = 0;
+        for ( int i=0; i< 32; i++ ){
+            int count = 0;
+            for ( Integer num : A){
+                if ( ( num & (1 << i)) != 0) {
+                    count++;
+                }
+            }
+            result = result | ((count%3) << i);
+        }
+        return result;
+    }
+}
