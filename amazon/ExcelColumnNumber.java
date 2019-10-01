5221a27d7480794b7298fadf6c0785da21757521
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-01 11:33:07 +0530

    amazon/ExcelColumnNumber.java

---
+++ amazon/ExcelColumnNumber.java
@@ -0,0 +1,17 @@
+package amazon;
+
+public class ExcelColumnNumber {
+    public int titleToNumber(String A) {
+        int result = 0;
+        int multiplyer = 1;
+        for ( int i=A.length()-1; i>=0; i--){
+            result+= ( multiplyer * ( A.charAt(i)-'A'+1) );
+            multiplyer*= 26;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        ExcelColumnNumber obj = new ExcelColumnNumber();
+        System.out.println(obj.titleToNumber("Z"));
+    }
+}
