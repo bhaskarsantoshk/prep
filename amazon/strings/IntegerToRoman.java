509390e76365e519851dab7543924b7b3df3ccee
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-03 19:28:30 +0530

    amazon/strings/IntegerToRoman.java

---
+++ amazon/strings/IntegerToRoman.java
@@ -0,0 +1,16 @@
+package amazon.strings;
+
+public class IntegerToRoman {
+    public String intToRoman(int A) {
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "IX", "X", "V", "IV", "I"};
+        StringBuilder result = new StringBuilder();
+        for ( int i=0; i< values.length; i++){
+            while ( A >= values[i]){
+                result.append(romans[i]);
+                A-= values[i];
+            }
+        }
+        return result.toString();
+    }
+}
