926db044bb4f2d8f983424d19271e71c3e4d41ac
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-07 17:01:58 +0530

    amazon/strings/PowerOf2.java

---
+++ amazon/strings/PowerOf2.java
@@ -0,0 +1,18 @@
+package amazon.strings;
+
+import java.math.BigInteger;
+
+public class PowerOf2 {
+    public int power(String A) {
+        BigInteger a = new BigInteger(A);
+        if ( a.compareTo(BigInteger.ONE) == 0 || a.compareTo(BigInteger.ZERO) == 0){
+            return 0;
+        }
+        for ( int i =0; i< a.bitLength()-1; i++){
+            if ( a.testBit(i)){
+                return 0;
+            }
+        }
+        return a.testBit(a.bitLength()-1) ? 1: 0;
+    }
+}
