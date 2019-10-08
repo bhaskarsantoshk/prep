4e0b4ceddf5ba826f97fc9c18b93af07b909fdbd
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-08 15:43:32 +0530

    amazon/linkedlists/ReorderList.java

--- amazon/linkedlists/ReorderList.java
+++ amazon/linkedlists/ReorderList.java
@@ -2,6 +2,43 @@ package amazon.linkedlists;
 
 public class ReorderList {
     public ListNode reorderList(ListNode A) {
-        return null;
+        if ( A == null || A.next == null ){
+            return A;
+        }
+        ListNode prev = null, slow= A, fast = A, l1 = A;
+        while ( fast != null && fast.next != null ){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l2 = reverse(slow);
+        merge(l1, l2);
+        return l1;
+    }
+
+    private void merge(ListNode l1, ListNode l2) {
+        while ( l1 != null ){
+            ListNode n1 = l1.next;
+            ListNode n2 = l2.next;
+            l1.next = l2;
+            if ( n1 == null ){
+                break;
+            }
+            l2.next = n1;
+            l1 = n1;
+            l2 = n2;
+        }
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, cur = head, next = null;
+        while ( cur!= null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
     }
 }
