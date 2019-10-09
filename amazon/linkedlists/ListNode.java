b234f90e4da46ad46f22dd629e92fd8aa9055b75
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-09 23:03:40 +0530

    amazon/linkedlists/ListNode.java

--- amazon/linkedlists/ListNode.java
+++ amazon/linkedlists/ListNode.java
@@ -3,5 +3,5 @@ package amazon.linkedlists;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x) { val = x; next = null; }
+    public ListNode(int x) { val = x; next = null; }
 }
