64367a3540070801a0c46c274e23e4466fbe7f7a
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-22 17:31:04 +0530

    dynamic_programming/dp/Example_6_1_LongestSubStringOfDigitswithEqualFirstAndSecondHalfSum.java:

--- dynamic_programming/dp/Example_6_1_LongestSubStringOfDigitswithEqualFirstAndSecondHalfSum.java
+++ dynamic_programming/dp/Example_6_1_LongestSubStringOfDigitswithEqualFirstAndSecondHalfSum.java
@@ -11,10 +11,9 @@ public class Example_6_1_LongestSubStringOfDigitswithEqualFirstAndSecondHalfSum 
                     continue;
                 }
                 int lsum = 0, rsum =0;
-                int start = i, end = j;
-                while ( start < end ){
-                    lsum+= s[start++] - '0';
-                    rsum+= s[end--] - '0';
+                for ( int k=0; k<len/2;k++){
+                    lsum+= (s[i+k] -'0');
+                    rsum+= (s[i+k+len/2] - '0')
                 }
                 if ( lsum == rsum ){
                     maxLen = len;
@@ -27,5 +26,25 @@ public class Example_6_1_LongestSubStringOfDigitswithEqualFirstAndSecondHalfSum 
     public static void main(String[] args){
         char[] s = "9430723".toCharArray();
         System.out.println(maxSubStringLength(s));
+    }
+
+    public static int maxSubStringLengthDP( char[] s){
+        int n = s.length;
+        int maxLen = 0;
+        int sum[][] = new int[n][n];
+        for ( int i=0; i<n; i++){
+            sum[i][i] = s[i]-'0';
+        }
+        for ( int len = 2; len<n; len++){
+            for ( int i=0; i< n-len+1; i++){
+                int j = i+len-1;
+                int k = len/2;
+                sum[i][j] = sum[i][j-k]+ sum[j-k+1][j];
+                if ( len% 2 == 0 && sum[i][j-k] == sum[j-k+1][j] && len > maxLen){
+                    maxLen = len;
+                }
+            }
+        }
+        return maxLen;
     }
 }
