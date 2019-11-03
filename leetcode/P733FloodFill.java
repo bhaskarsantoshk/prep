69997d9538b57a571bd79dc2c8fdac82f6a1f045
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-11-03 18:30:25 +0530

    leetcode/P733FloodFill.java

---
+++ leetcode/P733FloodFill.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class P733FloodFill {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if ( image[sr][sc] == newColor ){
+            return image;
+        }
+        dfs ( image, sr, sc, newColor, image[sr][sc]);
+        return image;
+    }
+
+    private void dfs(int[][] image, int i, int j, int newColor, int oldColor) {
+        if ( i <0 || i>= image.length || j < 0 || j>= image[0].length || image[i][j] != oldColor ){
+            return;
+        }
+        image[i][j] = newColor;
+        dfs ( image, i+1, j, newColor, oldColor);
+        dfs ( image, i, j+1, newColor, oldColor);
+        dfs ( image, i-1, j, newColor, oldColor);
+        dfs ( image, i, j-1, newColor, oldColor);
+    }
+}
