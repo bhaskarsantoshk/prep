749cdeb317c61d8dd452e2d6c1efac81ba5cfd13
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-11-10 18:49:22 +0530

    ctci/C16moderate/Intersection.java

--- ctci/C16moderate/Intersection.java
+++ ctci/C16moderate/Intersection.java
@@ -15,6 +15,7 @@ public class Intersection {
     }
 
     public Point intersection(Point start1, Point end1, Point start2, Point end2){
+        // Ordering
         if ( start1.x > end1.x){
             swap(start1, end1);
         }
@@ -24,6 +25,53 @@ public class Intersection {
         if ( start1.x > start2.x){
             swap(start1, start2);
             swap(end1, end2);
+        }
+
+        //Compute Lines
+        Line line1 = new Line(start1, end1);
+        Line line2 = new Line(start2, end2);
+
+        if ( line1.slope == line2.slope ){
+            if ( line1.yintercept == line2.yintercept && isBetween ( start1, start2, end1)){
+                return start2;
+            }
+            return null;
+        }
+
+        // Get intersection
+        // y = m1x + c1, y = m2x + c2 == > x = ( c2-c1)/ (m1-m2)
+        double x = (line2.yintercept - line1.yintercept) / (line1.slope - line2.slope);
+        // y = x * m + c
+        double y =  x * line1.slope + line1.yintercept;
+
+        Point intersection = new Point(x, y);
+
+        if ( isBetween(start1, intersection, end1) && isBetween(start2, intersection, end2)){
+            return intersection;
+        }
+        return null;
+    }
+
+    private boolean isBetween(Point start, Point middle, Point end) {
+        return isBetween(start.x, middle.x, end.x) && isBetween(start.y, middle.y, end.y);
+    }
+
+    private boolean isBetween(double start, double middle, double end) {
+        if ( start > end ){
+            return end <= middle && middle <= start;
+        }
+        else{
+            return start <= middle && middle <= end;
+        }
+    }
+
+    class Line{
+        double slope, yintercept;
+        Line ( Point start, Point end ){
+            double deltaX = end.y - start.y;
+            double deltaY = end.x - start.x;
+            slope = deltaY/deltaX;
+            yintercept = end.y - slope * end.x;
         }
     }
 
